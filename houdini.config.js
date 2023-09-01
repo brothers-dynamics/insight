595de5b71efd8782578ce627d4c0e056f2591ca1
catinrage <catinrage@gmail.com>
2023-09-01 17:35:53 +0330

    save

--- houdini.config.js
+++ houdini.config.js
@@ -2,11 +2,27 @@
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
-  include: 'src/**/*.{svelte,gql,ts,js}',
-  client: './src/houdini.ts',
-  plugins: {
-    'houdini-svelte': {}
-  }
+	schemaPath: 'src/**/*.graphql',
+
+	plugins: {
+		'houdini-svelte': {
+			client: 'src/lib/graphql/houdiniClient.ts'
+		}
+	},
+
+	scalars: {
+		DateTime: {
+			type: 'Date',
+			unmarshal(val) {
+				// to typescript type
+				return new Date(val);
+			},
+			marshal(date) {
+				// to graphql
+				return date.toISOString();
+			}
+		}
+	}
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
+import { kitql } from '@kitql/all-in';
 import { sveltekit } from '@sveltejs/kit/vite';
 import houdini from 'houdini/vite';
 import { createRequire } from 'node:module';
@@ -8,6 +9,7 @@ const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [
+    kitql(),
     houdini(),
     sveltekit(),
     ckeditor5({ theme: require.resolve('@ckeditor/ckeditor5-theme-lark') })
